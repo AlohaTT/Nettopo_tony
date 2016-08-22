14674db86a20bc249837ae69087ac6d7fd485931
Tony <1414596386@qq.com>
2016-08-22 10:49:30 +0800

    first

---
+++ NetTopo_ChunWu/src/org/deri/nettopo/display/Painter_3D.java
@@ -0,0 +1,144 @@
+package org.deri.nettopo.display;
+
+import java.util.Vector;
+
+import org.deri.nettopo.util.AttractorField;
+import org.deri.nettopo.util.Coordinate;
+import org.deri.nettopo.util.DistrictedArea;
+import org.eclipse.swt.graphics.RGB;
+
+public class Painter_3D implements Painter {
+
+	public void paintAllNodes() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void paintConnection(int id1, int id2, RGB rgb) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void paintConnection(int id1, int id2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void paintMobileNodes(double[][] path) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void paintNode(int id, RGB rgb) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void paintNode(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void paintNodeFocus(Coordinate c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void paintNodeFocus(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void paintNodes(int[] nodes) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void removeNode(Coordinate c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void removeNode(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void removeNodeFocus(Coordinate c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void removeNodeFocus(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void removeAllNodeFocus() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void rePaintAllNodes() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public String toString() {
+		String output = "3D has not been created";
+		System.out.println(output);
+		return output;
+	}
+
+	
+	public void paintAttractors(AttractorField fields) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void paintDistrictedArea(DistrictedArea area) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void removeAttractors(AttractorField fields) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void removeDistrictedArea(DistrictedArea area) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	public void removeDistrictedAreas(Vector<DistrictedArea> areas) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void paintDistrictedAreas(Vector<DistrictedArea> areas) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
